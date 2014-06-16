e78eb412f3f07b24000de4671d28aa0d1161595c
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2014-06-16 14:13:46 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='dropboxhandler',
+    version='1.0',
+    scripts='dropboxhandler.py'
+    description='Rename and sort incoming files from dropbox'
+)
