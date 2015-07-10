bbc98f3ed2c0716e778089061f9494e2afadef85
Adrian Seyboldt <adrian_seyboldt@web.de>
2015-07-10 16:36:47 +0200

    bump version to 1.5.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 2):
 
 setup(
     name='dropboxhandler',
-    version='1.5.3',
+    version='1.5.4',
     author='Adrian Seyboldt',
     author_email='adrian.seyboldt@web.de',
     url="https://github.com/qbicsoftware/dropboxhandler",
